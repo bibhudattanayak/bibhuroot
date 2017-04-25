39229054199a1ade0f194c1a89d3d04ccd5beb7f
bibhudattanayak <bibhudattanayak@gmail.com>
2017-04-25 21:40:09 +0530

    My First check in Adding maven

---
+++ src/main/java/com/WellsFargo/Software/App.java
@@ -0,0 +1,13 @@
+package com.WellsFargo.Software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
